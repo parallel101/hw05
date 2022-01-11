6a8f51d84d30f26bc4b37375767055fb9153a692
wangjy0109 <dswangjunye@gmail.com>
2022-01-11 17:39:56 -0500

    hw05

--- main.cpp
+++ main.cpp
@@ -31,10 +31,10 @@ std::string do_register(std::string username, std::string password, std::string 
     User user = {password, school, phone};
     std::string str;
     if (users.emplace(username, user).second) {
-        str = "register succeed";
+        str = "注册成功";
     }
     else {
-        str = "username has already been registered";
+        str = "用户名已被注册";
     }
     return str;
 }
@@ -47,20 +47,20 @@ std::string do_login(std::string username, std::string password) {
         auto sec = now - has_login.at(username);  // chrono算时间差
         using double_ms = std::chrono::duration<double, std::milli>;
         double elapsed = std::chrono::duration_cast<double_ms>(sec).count();
-        return std::to_string(elapsed) + " milliseconds within have logged in";
+        return std::to_string(elapsed) + "毫秒内登录过";
     }
     has_login[username] = now;
 
     {
         std::shared_lock grd(mtx1);
         if (users.find(username) == users.end()) {
-            return "wrong username";
+            return "用户名错误";
         }
         if (users.at(username).password != password) {
-            return "wrong password";
+            return "密码错误";
         }
     }
-    return "login success";
+    return "登录成功";
     
     
 }
@@ -70,9 +70,9 @@ std::string do_queryuser(std::string username) {
     if (users.find(username) != users.end()) {
         auto& user = users.at(username);
         std::stringstream ss;
-        ss << "username: " << username << std::endl;
-        ss << "school:" << user.school << std::endl;
-        ss << "phone: " << user.phone << std::endl;
+        ss << "用户名: " << username << std::endl;
+        ss << "学校:" << user.school << std::endl;
+        ss << "电话: " << user.phone << std::endl;
         return ss.str();
     }
     return "no such user";
@@ -94,11 +94,9 @@ ThreadPool tpool;
 
 
 namespace test {  // 测试用例？出水用力！
-//std::string username[] = {"张心欣", "王鑫磊", "彭于斌", "胡原名"};
-std::string username[] = { "zhang", "wang", "peng", "hu" };
+std::string username[] = {"张心欣", "王鑫磊", "彭于斌", "胡原名"};
 std::string password[] = {"hellojob", "anti-job42", "cihou233", "reCihou_!"};
-//std::string school[] = {"九百八十五大鞋", "浙江大鞋", "剑桥大鞋", "麻绳理工鞋院"};
-std::string school[] = { "jiubawu", "zhejiang", "jianqiao", "mashengligong" };
+std::string school[] = {"九百八十五大鞋", "浙江大鞋", "剑桥大鞋", "麻绳理工鞋院"};
 std::string phone[] = {"110", "119", "120", "12315"};
 }
 
