80c248a615518de74621b4e517407b6c47dbe542
qyc19961115 <atom_qu@126.com>
2022-01-12 23:57:58 +0800

    FinishHW05

--- main.cpp
+++ main.cpp
@@ -1,89 +1,152 @@
-// 小彭老师作业05：假装是多线程 HTTP 服务器 - 富连网大厂面试官觉得很赞
-#include <functional>
+﻿#include <functional>
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
 #include <string>
 #include <thread>
 #include <map>
+#include <chrono>
+#include <shared_mutex>
+#include <mutex>
+
+
+template<typename T>
+class  MTMap
+{
+public:
+	std::map<std::string, T> _res;
+	mutable std::shared_mutex _mtx;
+
+	auto emplace(std::string str, T t)
+	{
+		std::unique_lock qrd(_mtx);
+		return _res.emplace(str, t);
+	}
+
+	bool find(std::string str)
+	{
+		std::shared_lock qrd(_mtx);
+		return _res.find(str) != _res.end();
+	}
+	T at(std::string str)
+	{
+		std::shared_lock qrd(_mtx);
+		return _res.at(str);
+	}
+	void set(std::string str, T t)
+	{
+		std::unique_lock qrd(_mtx);
+		_res[str] = t;
+	}
+};
+
 
 
 struct User {
-    std::string password;
-    std::string school;
-    std::string phone;
+	std::string password;
+	std::string school;
+	std::string phone;
 };
 
-std::map<std::string, User> users;
-std::map<std::string, long> has_login;  // 换成 std::chrono::seconds 之类的
+
+MTMap<User> users;
+//std::map<std::string, User> users;
+MTMap<std::chrono::steady_clock::time_point> has_login;
+//std::map<std::string, std::chrono::steady_clock::time_point> has_login;  // 换成 std::chrono::seconds 之类的
+
+
 
 // 作业要求1：把这些函数变成多线程安全的
 // 提示：能正确利用 shared_mutex 加分，用 lock_guard 系列加分
-bool do_register(std::string username, std::string password, std::string school, std::string phone) {
-    User user = {password, school, phone};
-    if (users.emplace(username, user).second)
-        return "注册成功";
-    else
-        return "用户名已被注册";
+std::string do_register(std::string username, std::string password, std::string school, std::string phone) {
+	User user = { password, school, phone };
+	if (users.find(username))
+	{
+		return "已注册";
+	}
+	else {
+		users.emplace(username, user);
+		return "未注册，已添加";
+	}
+
 }
 
 std::string do_login(std::string username, std::string password) {
-    // 作业要求2：把这个登录计时器改成基于 chrono 的
-    long now = time(NULL);   // C 语言当前时间
-    if (has_login.find(username) != has_login.end()) {
-        int sec = now - has_login.at(username);  // C 语言算时间差
-        return std::to_string(sec) + "秒内登录过";
-    }
-    has_login[username] = now;
+	// 作业要求2：把这个登录计时器改成基于 chrono 的
 
-    if (users.find(username) == users.end())
-        return "用户名错误";
-    if (users.at(username).password != password)
-        return "密码错误";
-    return "登录成功";
+
+	//long now = time(NULL);   // C 语言当前时间
+	auto now = std::chrono::steady_clock::now();
+	if (has_login.find(username)) {
+		auto duration = now - has_login.at(username);
+		int64_t sec = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+		return std::to_string(sec) + "ms内登录过";
+	}
+	has_login.set(username, now);
+	if (!users.find(username)) {
+		return  "用户名错误";
+	}
+	if (users.at(username).password != password)
+		return "密码错误";
+	return "登录成功";
 }
 
 std::string do_queryuser(std::string username) {
-    auto &user = users.at(username);
-    std::stringstream ss;
-    ss << "用户名: " << username << std::endl;
-    ss << "学校:" << user.school << std::endl;
-    ss << "电话: " << user.phone << std::endl;
-    return ss.str();
+
+	if (!users.find(username)) return "该用户名不存在";
+	auto user = users.at(username);
+	std::stringstream ss;
+	ss << "用户名: " << username << std::endl;
+	ss << "学校:" << user.school << std::endl;
+	ss << "电话: " << user.phone << std::endl;
+	return ss.str();
 }
 
 
 struct ThreadPool {
-    void create(std::function<void()> start) {
-        // 作业要求3：如何让这个线程保持在后台执行不要退出？
-        // 提示：改成 async 和 future 且用法正确也可以加分
-        std::thread thr(start);
-    }
+
+	std::vector<std::unique_ptr<std::thread>> _vecThread;
+
+	void create(std::function<void()> start) {
+		// 作业要求3：如何让这个线程保持在后台执行不要退出？
+		// 提示：改成 async 和 future 且用法正确也可以加分
+		auto thr = std::make_unique<std::thread>(start);
+		_vecThread.push_back(std::move(thr));
+	}
+
+	~ThreadPool()
+	{
+		for (auto& i : _vecThread)
+		{
+			i->join();
+		}
+	}
+
 };
 
 ThreadPool tpool;
 
 
 namespace test {  // 测试用例？出水用力！
-std::string username[] = {"张心欣", "王鑫磊", "彭于斌", "胡原名"};
-std::string password[] = {"hellojob", "anti-job42", "cihou233", "reCihou_!"};
-std::string school[] = {"九百八十五大鞋", "浙江大鞋", "剑桥大鞋", "麻绳理工鞋院"};
-std::string phone[] = {"110", "119", "120", "12315"};
+	std::string username[] = { "张心欣", "王鑫磊", "彭于斌", "胡原名" };
+	std::string password[] = { "hellojob", "anti-job42", "cihou233", "reCihou_!" };
+	std::string school[] = { "九百八十五大鞋", "浙江大鞋", "剑桥大鞋", "麻绳理工鞋院" };
+	std::string phone[] = { "110", "119", "120", "12315" };
 }
 
 int main() {
-    for (int i = 0; i < 262144; i++) {
-        tpool.create([&] {
-            std::cout << do_register(test::username[rand() % 4], test::password[rand() % 4], test::school[rand() % 4], test::phone[rand() % 4]) << std::endl;
-        });
-        tpool.create([&] {
-            std::cout << do_login(test::username[rand() % 4], test::password[rand() % 4]) << std::endl;
-        });
-        tpool.create([&] {
-            std::cout << do_queryuser(test::username[rand() % 4]) << std::endl;
-        });
-    }
+	for (int i = 0; i < 1000; i++) {
+		tpool.create([&] {
+			std::cout << do_register(test::username[rand() % 4], test::password[rand() % 4], test::school[rand() % 4], test::phone[rand() % 4]) << std::endl;
+		});
+		tpool.create([&] {
+			std::cout << do_login(test::username[rand() % 4], test::password[rand() % 4]) << std::endl;
+		});
+		tpool.create([&] {
+			std::cout << do_queryuser(test::username[rand() % 4]) << std::endl;
+		});
+	}
 
-    // 作业要求4：等待 tpool 中所有线程都结束后再退出
-    return 0;
+	// 作业要求4：等待 tpool 中所有线程都结束后再退出
+	return 0;
 }
