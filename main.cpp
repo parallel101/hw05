f96938b470c343cba749f3fa2ab3c5bf1301d9d7
jinjiwu <3426377882@qq.com>
2022-01-15 02:34:09 +0800

    multi-thread

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 #include <string>
 #include <thread>
 #include <map>
-
+#include <mutex>
+#include <shared_mutex>
+#include <vector>
 
 struct User {
     std::string password;
@@ -16,11 +18,17 @@ struct User {
 
 std::map<std::string, User> users;
 std::map<std::string, long> has_login;  // 换成 std::chrono::seconds 之类的
+// std::map<std::string,std::chrono::seconds> has_login;
+
+std::mutex users_mtx;
+std::mutex login_mtx;
+// std::shared_mutex shared_mtx;
 
 // 作业要求1：把这些函数变成多线程安全的
 // 提示：能正确利用 shared_mutex 加分，用 lock_guard 系列加分
 std::string do_register(std::string username, std::string password, std::string school, std::string phone) {
     User user = {password, school, phone};
+    auto grd=std::unique_lock<std::mutex>{users_mtx};
     if (users.emplace(username, user).second)
         return "注册成功";
     else
@@ -30,6 +38,7 @@ std::string do_register(std::string username, std::string password, std::string 
 std::string do_login(std::string username, std::string password) {
     // 作业要求2：把这个登录计时器改成基于 chrono 的
     long now = time(NULL);   // C 语言当前时间
+    auto grd=std::unique_lock<std::mutex>{login_mtx};
     if (has_login.find(username) != has_login.end()) {
         int sec = now - has_login.at(username);  // C 语言算时间差
         return std::to_string(sec) + "秒内登录过";
@@ -44,6 +53,7 @@ std::string do_login(std::string username, std::string password) {
 }
 
 std::string do_queryuser(std::string username) {
+    auto grd=std::unique_lock<std::mutex>{users_mtx};
     auto &user = users.at(username);
     std::stringstream ss;
     ss << "用户名: " << username << std::endl;
@@ -54,10 +64,19 @@ std::string do_queryuser(std::string username) {
 
 
 struct ThreadPool {
+    std::vector<std::thread> pools;
+
     void create(std::function<void()> start) {
         // 作业要求3：如何让这个线程保持在后台执行不要退出？
         // 提示：改成 async 和 future 且用法正确也可以加分
-        std::thread thr(start);
+        // std::thread thr(start);
+        pools.push_back(std::thread{start});
+    }
+
+    void join(){
+        for (auto & t:pools){
+            t.join();
+        }
     }
 };
 
@@ -85,5 +104,7 @@ int main() {
     }
 
     // 作业要求4：等待 tpool 中所有线程都结束后再退出
+    tpool.join();
+
     return 0;
 }
